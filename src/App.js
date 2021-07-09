a70f90cd9ae3e988929ba4aa63b22706f4da3cd4
sunpra1 <49110481+sunpra1@users.noreply.github.com>
2021-07-09 17:07:23 +0545

    Navbar has been incoporated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import HomePage from './components/pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container-fluid bg-dark">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../layout/NavBar';
+
+export default function Home() {
+    return (
+        <>
+            <NavBar />
+            <div className="container-fluid">
+                
+            </div>
+        </>        
+    )
+}
