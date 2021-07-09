74ba786d09da4c37f3c110eb7f0cddb936463cec
sunpra1 <49110481+sunpra1@users.noreply.github.com>
2021-07-09 18:44:25 +0545

    List items UI improved

--- src/components/pages/Home/ListItem.js
+++ src/components/pages/Home/ListItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faMap, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 export default function ListItem(props) {
 
     const { value } = props;
 
     return (
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="ms-2 me-auto">
-                <div className="fw-bold">{value.name}</div>
-                {value.country}
-            </div>
-            <span className="badge bg-primary rounded-pill">{value.phoneNumber}</span>
+        <li className="list-group-item">
+            <h6 className="text-info">{value.name}</h6>
+            <p className="text-secondary" style={{ fontSize: "13px" }}>
+                <span className="text-danger"><FontAwesomeIcon icon={faHeart} /></span> FAVOURITE BRAND: {value.favouritePhoneBrand}
+                <span className="text-dark ms-4"><FontAwesomeIcon icon={faMap} /></span> COUNTRY: {value.country}
+                <span className="text-primary ms-4"><FontAwesomeIcon icon={faPhone} /></span> PHONE NUMBER: {value.phoneNumber}
+            </p>
         </li>
     );
 }
